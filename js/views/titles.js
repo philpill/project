cfe5a516b1db3f0c3eef0fbe938d5062e7bb2330
philpill <github@philpill.net>
2013-06-20 04:10:30 +0100

    fixing titles view

--- js/views/titles.js
+++ js/views/titles.js
@@ -1,36 +1,36 @@
-define([
-	'marionette',
-	'views/title',
-	'collections/title',
-	'sinon',
-	'mock'
-	], function(Marionette, TitleView, TitleCollection, undefined, Mock){
+define(function(require){
+
+		var Marionette = require('marionette');
+		var TitleView = require('views/title');
+		var TitleCollection = require('collections/title');
+		var Mock = require('mock');
+
+		require('sinon');
 
 	console.log(Mock);
 
-	var titleCollection = new TitleCollection();
-
 	var server = sinon.fakeServer.create();
 
-	var TitlesView = Backbone.Marionette.CompositeView.extend({
+	var titleCollection = new TitleCollection();
+
+			server.respondWith("GET", "/gametitles/list",
+				[200, {"Content-Type": "application/json"},
+				Mock.titlesResponse]);
+	titleCollection.fetch();
+
+	server.respond();
+
+
+	var TitlesView = Backbone.Marionette.CollectionView.extend({
 		template: "#TitlesTemplate",
 		tagName: 'section',
 		className: 'titles',
 		itemView: TitleView,
 		collection: titleCollection,
-		initialize: function() {
-
-			server.respondWith("GET", "/gametitles/list",
-				[200, {"Content-Type": "application/json"},
-				Mock.titlesResponse]);
-
-			this.collection.fetch();
-
-			server.respond();
-
-			server.restore();
-		},
 		appendHtml: function(collectionView, itemView, index){
+			console.log('appendHtml()');
+			console.log(itemView);
+			console.log($(itemView.el));
 			collectionView.$("dl").append($(itemView.el).html());
 		}
 	});
