673167cc09645a71cb3928c2d04736fd139ca036
philpill <github@philpill.net>
2013-06-20 13:50:50 +0100

    delete title

--- js/views/titles.js
+++ js/views/titles.js
@@ -10,6 +10,23 @@ define(function(require){
 	var TitlesView = Marionette.CompositeView.extend({
 		template: "#TitlesTemplate",
 		itemView: TitleView,
+		events: {
+			'click .delete' : 'deleteTitle'
+		},
+		deleteTitle: function(e) {
+			console.log('deleteTitle()');
+			e.preventDefault();
+			var $currentTarget = $(e.currentTarget);
+			var titleId = $currentTarget.parents('dt:eq(0)').data('id');
+			console.log(titleId);
+			console.log(this.collection);
+
+			var title = this.collection.get(titleId)
+			console.log(title);
+
+			this.collection.remove(title);
+			console.log(this.collection);
+		},
 		appendHtml: function(collectionView, itemView, index){
 			collectionView.$('dl').append($(itemView.el).html());
 		}
